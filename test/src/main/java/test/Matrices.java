0942f63775055a0e1a29f6fbb9e46a01419c8079
somozadev <somozadev@gmail.com>
2021-02-09 19:11:10 +0100

    Teacher´s method and own method

--- test/src/main/java/test/Matrices.java
+++ test/src/main/java/test/Matrices.java
@@ -1,5 +1,8 @@
 package test;
 
+import javax.swing.DebugGraphics;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 public class Matrices {
 
     /* SUSPENSO INMEDIATO > SUSPENDE EL BUCLE AL METER UN RETURN EN MITAD */
@@ -13,6 +16,26 @@ public class Matrices {
         return false;
     }
 
+    public static boolean FindRightTeacher(int[][]matrix, int data)
+    {
+        boolean finded = false;
+        int row = 0; 
+        int col = 0; 
+
+        while(!finded && row < matrix.length)
+        {
+            while(!finded && col < matrix[row].length)
+            {
+                finded = (data == matrix[row][col]);
+
+                System.out.println(matrix[row][col]);
+                ++col;
+            }
+            col = 0;
+            ++row;
+        }
+        return finded;
+    }
 
     //UN SOLO WHILE... ES MAS O MENOS COSTOSO ?
     public static boolean FindRight(int[][] matrix, int data) {
@@ -26,7 +49,7 @@ public class Matrices {
                 finded = true;
 
             ++col;
-            
+
             if(col == matrix[row].length)
             {
                 col = 0;
@@ -40,7 +63,7 @@ public class Matrices {
     public static void main(String[] args) {
         int num = 0;
         int[][] matrix = { { 1, 2, 4, 6, 3 }, { 5, 7, 8, 0 } };
-        System.out.println("Resultado: " + FindRight(matrix, num));
+        System.out.println("Resultado: " + FindRightTeacher(matrix, num));
 
     }
 
