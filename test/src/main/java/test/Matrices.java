48836c22f143fd951e3ef2b5d0e00d6467e4c0d4
somozadev <somozadev@gmail.com>
2021-02-09 18:30:57 +0100

    first commit

---
+++ test/src/main/java/test/Matrices.java
@@ -0,0 +1,47 @@
+package test;
+
+public class Matrices {
+
+    /* SUSPENSO INMEDIATO > SUSPENDE EL BUCLE AL METER UN RETURN EN MITAD */
+    public static boolean Find(int[][] matrix, int data) {
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[x].length; y++) {
+                if (data == matrix[x][y])
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+    //UN SOLO WHILE... ES MAS O MENOS COSTOSO ?
+    public static boolean FindRight(int[][] matrix, int data) {
+        int row = 0;
+        int col = 0;
+        boolean finded = false;
+
+        while (!finded && row < matrix.length && col < matrix[row].length) {  
+
+            if (data == matrix[row][col])
+                finded = true;
+
+            ++col;
+            
+            if(col == matrix[row].length)
+            {
+                col = 0;
+                ++row;
+            }
+        }
+
+        return finded;
+    }
+
+    public static void main(String[] args) {
+        int num = 0;
+        int[][] matrix = { { 1, 2, 4, 6, 3 }, { 5, 7, 8, 0 } };
+        System.out.println("Resultado: " + FindRight(matrix, num));
+
+    }
+
+}
